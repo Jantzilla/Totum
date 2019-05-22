a525abc7c95beb665f7d53ec64a836c6890d2db4
Jantz <jantzillac.93@gmail.com>
2019-05-21 20:25:04 -0500

    Declare & initialize "bottomNavigationItemView"

--- app/src/main/java/com/creativesource/totum/MainActivity.kt
+++ app/src/main/java/com/creativesource/totum/MainActivity.kt
@@ -2,11 +2,13 @@ package com.creativesource.totum
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationItemView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationItemView = findViewById<BottomNavigationItemView>(R.id.bottom_navigation)
     }
 }
